6ef0876198771ed6a5691688d54a9643104da1e5
きたたか <usgreenfox@gmail.com>
2021-03-19 11:41:28 +0900

    added view and template for detailed blog post as well as css for the site.

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,13 @@ from .models import Post
 from django.utils import timezone
 #  . はカレントディレクトリorカレントアプリケーション
 # views.py ,models.py が同じディレクトリにあるから可能
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
   posts =Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
   return render(request, 'blog/post_list.html',{'posts':posts})
+
+def post_detail(request,pk):
+  post = get_object_or_404(Post,pk=pk)
+  return render(request, 'blog/post_detail.html',{'html':post})
   
